b0b162e7046534b41017977c31680cb947e0235d
Aman Nigam <amannigam18082001@gmail.com>
2024-04-15 04:56:00 -0700

    ds

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -6,6 +6,8 @@ import { User } from "../models/user.model.js";
 
 const isAuthenticated = TryCatch(async (req, res, next) => {
   const { token } = req.cookies;
+
+  console.log(token);
   
 
   if (!token) {
